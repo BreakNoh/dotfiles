4dac6b00e3281983b075dfc190a9b708fa14f3c4
Breno <brematosmene@gmail.com>
2026-03-24 22:23:02 -0300

    adiciona zsh

--- install.sh
+++ install.sh
@@ -21,8 +21,20 @@ ln -sf "$DOTFILES/nvim" "$CONFIG/nvim"
 # tmux
 # instala tpm se não tiver instalado
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-    git clone -q https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+	git clone -q https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 backup "$HOME/.tmux.conf"
 ln -sf "$DOTFILES/tmux/tmux.conf" "$HOME/.tmux.conf"
+
+# zsh
+# instala oh-my-zsh se não tiver instalado
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+backup "$HOME/.zshrc"
+backup "$HOME/.zprofile"
+
+ln -sf "$DOTFILES/zsh/zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES/zsh/profile" "$HOME/.zprofile"
