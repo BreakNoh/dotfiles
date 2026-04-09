6ddf5bc19a05508647b9066f702eb1a5f5e22497
BreaKnoh <brematosmene@gmail.com>
2026-04-09 08:29:14 -0300

    remove atalho ga

--- nvim/lua/config/atalhos.lua
+++ nvim/lua/config/atalhos.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action, {})
+-- vim.keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action, {})
 vim.keymap.set("n", "<Leader>gd", vim.lsp.buf.definition, {})
 
 -- Compatibilidade com TMUX
--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -35,6 +35,7 @@ return {
 			css = { "prettierd", "prettier" },
 
 			sh = { "beautysh" },
+			bash = { "beautysh" },
 			zsh = { "beautysh" },
 		},
 		default_format_opts = {
@@ -42,61 +43,9 @@ return {
 			stop_after_first = true,
 		},
 		format_on_save = { timeout_ms = 500 },
-		formatters = {
-			shfmt = {
-				append_args = { "-i", "2" },
-			},
-		},
 	},
 	init = function()
 		-- If you want the formatexpr, here is the place to set it
 		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 	end,
-	-- "stevearc/conform.nvim",
-	--
-	-- opts = {
-	-- 	log_level = vim.log.levels.DEBUG,
-	-- 	formatters = {
-	-- 		prettier = {
-	-- 			prepend_args = { "--tab-width", "4" },
-	-- 		},
-	-- 	},
-	--
-	-- 	format_on_save = function(bufnr)
-	-- 		if vim.bo[bufnr].filetype == "svelte" then
-	-- 			return { timeout_ms = 2000, lsp_fallback = false }
-	-- 		end
-	-- 		return { timeout_ms = 500, lsp_fallback = true }
-	-- 	end,
-	--
-	-- 	formatters_by_ft = {
-	-- 		javascript = { "prettierd", "prettier", stop_after_first = true },
-	-- 		typescript = { "prettierd", "prettier", stop_after_first = true },
-	-- 		svelte = { "prettierd", "prettier", stop_after_first = true },
-	-- 		html = { "prettierd", "prettier", stop_after_first = true },
-	-- 		css = { "prettierd", "prettier", stop_after_first = true },
-	--
-	-- 		typst = { "prettypst" },
-	-- 		markdown = { "prettierd", "prettier", stop_after_first = true },
-	-- 		json = { "prettierd", "prettier", stop_after_first = true },
-	-- 		jsonc = { "prettierd", "prettier", stop_after_first = true },
-	--
-	-- 		lua = { "stylua" },
-	-- 		python = { "ruff_format" },
-	--
-	-- 		rust = { "rustfmt", lsp_format = "fallback" },
-	-- 		toml = { "tombi" },
-	-- 	},
-	-- },
-	-- -- config = true,
-	--
-	-- keys = {
-	-- 	{
-	-- 		"<leader>fm",
-	-- 		function(_)
-	-- 			require("conform").format()
-	-- 		end,
-	-- 		desc = "Conform Format",
-	-- 	},
-	-- },
 }
